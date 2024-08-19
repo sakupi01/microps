6a34e03a6ea62b61ecc1654a575f32bc0cfef88c
sakura-adachi <sakura-adachi@cybozu.co.jp>
2024-08-19 23:22:59 +0900

    デバイスの管理まで

---
+++ driver/dummy.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/time.h>
+
+#include "platform.h"
+
+#include "util.h"
+#include "net.h"
+
+#define NET_DEVICE_TYPE_DUMMY 0x0003
+#define DUMMY_MTU 1500
+
+static int dummy_transmit(struct net_device *dev, uint16_t type, const uint8_t *data, size_t len, const void *dst)
+{
+    debugf("dev=%s, type=0x%04x, len=%zu", dev->name, type, len);
+    debugdump(data, len);
+    /* drop data */
+    return 0;
+}
+
+static struct net_device_ops dummy_ops = {
+    .transmit = dummy_transmit,
+};
+
+struct net_device * dummy_init(void)
+{
+    struct net_device *dev;
+
+    dev = net_device_alloc();
+    if (!dev) {
+        errorf("net_device_alloc() failure");
+        return NULL;
+    }
+    dev->type = NET_DEVICE_TYPE_DUMMY;
+    dev->mtu = DUMMY_MTU;
+    dev->hlen = 0; /* non header */
+    dev->alen = 0; /* non address */
+    dev->ops = &dummy_ops;
+    if (net_device_register(dev) == -1) {
+        errorf("net_device_register() failure");
+        return NULL;
+    }
+    debugf("initialized, dev=%s", dev->name);
+    return dev;
+}
--- net.c
+++ net.c
@@ -1,27 +1,44 @@
-struct net_device {
-    struct net_device *next;
-    unsigned int index;
-    char name[IFNAMSIZ];
-    uint16_t type;
-    uint16_t mtu;
-    uint16_t flags;
-    uint16_t hlen; /* header length */
-    uint16_t alen; /* address length */
-    uint8_t addr[NET_DEVICE_ADDR_LEN];
-    union {
-        uint8_t peer[NET_DEVICE_ADDR_LEN];
-        uint8_t broadcast[NET_DEVICE_ADDR_LEN];
-    };
-    struct net_device_ops *ops;
-    void *priv;
-};
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/time.h>
 
-// net_deviceに実装されている関数のポインタを持つ構造体（transmit（送信関数）は重要）
-struct net_device_ops {
-    int (*open)(struct net_device *dev);
-    int (*close)(struct net_device *dev);
-    int (*transmit)(struct net_device *dev, uint16_t type, const uint8_t *data, size_t len, const void *dst);
-};
+#include "platform.h"
+
+#include "util.h"
+#include "net.h"
+
+// デバイスリスト
+static struct net_device *devices;
+
+// デバイス構造体のサイズのメモリを確保
+struct net_device * net_device_alloc(void)
+{
+    struct net_device *dev;
+
+    dev = memory_alloc(sizeof(*dev));
+    if (!dev) {
+        errorf("memory_alloc() failure");
+        return NULL;
+    }
+    return dev;
+}
+
+// デバイス構造体のメモリをデバイスリストの先頭に追加
+int net_device_register(struct net_device *dev)
+{
+    static unsigned int index = 0;
+
+    dev->index = index++;
+    snprintf(dev->name, sizeof(dev->name), "net%d", dev->index);
+    dev->next = devices;
+    devices = dev;
+    infof("registered, dev=%s, type=0x%04x", dev->name, dev->type);
+    return 0;
+}
 
 // ネットワークデバイスの状態を確認して、openじゃない場合はopenする
 static int net_device_open(struct net_device *dev)
