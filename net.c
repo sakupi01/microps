648c4bffc0fce80a4fad5e3165f0571efa6c3e74
sakura-adachi <sakura-adachi@cybozu.co.jp>
2024-08-19 21:39:14 +0900

    デバイスの生成と登録

---
+++ net.c
@@ -0,0 +1,29 @@
+// デバイスリスト
+static struct net_device *devices;
+
+// デバイス構造体のサイズのメモリを確保
+struct net_device * net_device_alloc(void)
+{
+    struct net_device *dev;
+
+    dev = memory_alloc(sizeof(*dev));
+    if (!dev) {
+        errorf("memory_alloc() failure");
+        return NULL;
+    }
+    return dev;
+}
+
+// デバイス構造体のメモリをデバイスリストの先頭に追加
+int net_device_register(struct net_device *dev)
+{
+    static unsigned int index = 0;
+
+    dev->index = index++;
+    snprintf(dev->name, sizeof(dev->name), "net%d", dev->index);
+    dev->next = devices;
+    devices = dev;
+    infof("registered, dev=%s, type=0x%04x", dev->name, dev->type);
+    return 0;
+}
+
